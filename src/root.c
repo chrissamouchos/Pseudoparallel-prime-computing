03d4add22151dcc7cf1af3c693bd6e3d7747a30f
chrissamouchos <samouchos98@hotmail.com>
2020-11-07 23:26:03 +0200

    vasia2

--- src/root.c
+++ src/root.c
@@ -16,25 +16,26 @@ int main(int argc, char** argv){
 	status = mkfifo(root_node, 0666);
 	if(status < 0) perror("Pipe creation error");
 
-	// char buf[] = "Hello from parent!\n";
+	char* buff = malloc(sizeof(char)*20);
+	buff = "Hello from parent!\n";
 
-	// if((pid = fork()) < -1){
-	// 	perror("Fork error");
-	// 	exit(1);
-	// }
+	if((pid = fork()) < -1){
+		perror("Fork error");
+		exit(1);
+	}
 
-	// if(pid == 0){
-	// 	printf("Creating inner_node...\n");
-	// 	execl("./inner_node", "inner_node", NULL);
-	// }
-	// else{
-	// 	printf("I am parent!\n");
-	// 	if(fd = open("root_node", O_WRONLY) < 0)
-	// 		perror("Error open pipe");
-	// 	if(fd = write(fd, buf, strlen(buf)) != strlen(buf))
-	// 		perror("Error write in pipe");
-	// 	wait(NULL);
-	// }
-	// close(fd);
+	if(pid == 0){
+		printf("Creating inner_node...\n");
+		execl("./inner_node", "inner_node", NULL);
+	}
+	else{
+		printf("I am parent!\n");
+		if(fd = open("root_node", O_WRONLY) < 0)
+			perror("Error open pipe");
+		if(fd = write(fd, buf, strlen(buf)) != strlen(buf))
+			perror("Error write in pipe");
+		wait(NULL);
+	}
+	close(fd);
 	return 0;
 }
