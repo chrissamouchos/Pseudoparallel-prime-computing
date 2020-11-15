4e21a3371ff0c5448214f7b18ec113132b08cece
chrissamouchos <samouchos98@hotmail.com>
2020-11-15 16:40:50 +0200

    Prime functions

--- include/Find_primes.h
+++ include/Find_primes.h
@@ -1,6 +1,8 @@
 /*Library that contains the thee functions that returns a prime*/
 #pragma once
 
+int modular_pow(unsigned int base, unsigned int exponent, unsigned int mod); /*efficient power function*/
+
 int prime_1(int n);	/*Search for divisors across all numbers*/
 
 int prime_2(int n);	/*Search for divisors until square root of n*/
--- src/Find_primes.c
+++ src/Find_primes.c
@@ -7,6 +7,22 @@
 #define TRUE 1
 #define FALSE 0
 
+int modular_pow(unsigned int base, unsigned int exponent, unsigned int mod){
+  int result = 1;
+  base = base % mod;                              // Update x if it is more than or  equal to p
+  if (base == 0)                                  // In case x is divisible by p;
+    return 0;
+
+  while(exponent > 0){
+    	if (exponent & 1)                             //This action is equivalent to exponent % 2
+      		result = (result*base) % mod;
+
+    	exponent = exponent >> 1;                     //This action is equivalent to exponent = exponent/2
+    	base = (base*base) % mod;
+    }
+    return result;
+}
+
 int prime_1(int n){
 	int i;
 	if(n == 1)	return FALSE;
@@ -14,6 +30,7 @@ int prime_1(int n){
 		if( n % i == 0)
 			return FALSE;
 	return TRUE;
+}
 
 int prime_2(int n){
 	int i = 0, limitup = 0;
@@ -26,5 +43,9 @@ int prime_2(int n){
 }
 
 int my_prime(int n){
-	
+	int flag = -1, a = 2;
+	if(n % 2 == 0) return FALSE;
+	flag = modular_pow(a, n-1, n);
+	if(flag == 1) return TRUE;
+	else return FALSE;
 }
